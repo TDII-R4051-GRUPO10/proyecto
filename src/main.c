d3ddd48a41b49df83768851c0e17d4e58611a75c
Pablo Fritzler <tamdamian@hotmail.com>
2018-09-22 19:26:21 -0300

    Auto stash before merge of "master" and "origin/master"

--- src/main.c
+++ src/main.c
@@ -1,111 +1,151 @@
-/* Copyright 2015, Pablo Ridolfi
- * All rights reserved.
- *
- * This file is part of lpc1769_template.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice,
- *    this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright notice,
- *    this list of conditions and the following disclaimer in the documentation
- *    and/or other materials provided with the distribution.
- *
- * 3. Neither the name of the copyright holder nor the names of its
- *    contributors may be used to endorse or promote products derived from this
- *    software without specific prior written permission.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- * POSSIBILITY OF SUCH DAMAGE.
- *
- */
+/*
+===============================================================================
+ Name        : Proyecto.c
+ Author      : $(author)
+ Version     :
+ Copyright   : $(copyright)
+ Description : main definition
+===============================================================================
+*/
 
-/** @brief Blinky using FreeRTOS.
- *
- *
- * NOTE: It's interesting to check behavior differences between standard and
- * tickless mode. Set @ref configUSE_TICKLESS_IDLE to 1, increment a counter
- * in @ref vApplicationTickHook and print the counter value every second
- * inside a task. In standard mode the counter will have a value around 1000.
- * In tickless mode, it will be around 25.
- *
- */
+#ifdef __USE_CMSIS
+#include "LPC17xx.h"
+#endif
 
-/** \addtogroup rtos_blink FreeRTOS blink example
- ** @{ */
 
-/*==================[inclusions]=============================================*/
+#include <cr_section_macros.h>
 
 #include "board.h"
-
 #include "FreeRTOS.h"
-#include "FreeRTOSConfig.h"
 #include "task.h"
+#include "queue.h"
 #include "semphr.h"
-
-#include "main.h"
+#include "FW_GPIO.h"
+#include "Parcial.h"
 #include "header.h"
+#include <stdlib.h>
 
 
-xSemaphoreHandle init;
-xSemaphoreHandle motor1;
-xSemaphoreHandle motor2;
-xSemaphoreHandle alarma;
-xSemaphoreHandle sensor_color;
-xSemaphoreHandle sd;
-xSemaphoreHandle motor1_ISR;
+xSemaphoreHandle 	init;
+xSemaphoreHandle 	motor1;
+xSemaphoreHandle 	motor2;
+xSemaphoreHandle 	alarma;
+xSemaphoreHandle 	sensor_color;
+xSemaphoreHandle 	sd;
+xSemaphoreHandle 	motor1_ISR;
+
+xSemaphoreHandle 	init_OK;
+xSemaphoreHandle 	motor1_OK;
+xSemaphoreHandle 	motor2_OK;
+xSemaphoreHandle 	alarma_OK;
+xSemaphoreHandle 	sd_OK;
+
+xQueueHandle 		color;
+xQueueHandle 		posicion;
+xQueueHandle 		contador;
 
 
-xSemaphoreHandle init_OK;
-xSemaphoreHandle motor1_OK;
-xSemaphoreHandle motor2_OK;
-xSemaphoreHandle alarma_OK;
-xSemaphoreHandle sd_OK;
-
-xQueueHandle color;
-xQueueHandle posicion;
-xQueueHandle contador;
-
-/*==================[macros and definitions]=================================*/
-
-/*==================[internal data declaration]==============================*/
-
-/*==================[internal functions declaration]=========================*/
-
-/** @brief hardware initialization function
- *	@return none
- */
 static void initHardware(void);
 
-/*==================[internal data definition]===============================*/
 
-/*==================[external data definition]===============================*/
-
-/*==================[internal functions definition]==========================*/
 
 static void initHardware(void)
 {
     SystemCoreClockUpdate();
 
-    Board_Init();
 
-    Board_LED_Set(0, false);
+    SetPINSEL(MOTOR,0);
+
+    SetDIR(MOTOR,0);
+
+
 }
 
 
 
-static void Inicializacion ()
+static void Tarea_Ppal(void *p)
+{
+	static int estado=INICIO;
+	static int data=NON;
+	static int cont=0;
+	static int i;
+
+	static int colores[5];
+
+	for(i=0;i<5;i++)
+		colores[i]=0;
+
+	while(1)
+	{
+		switch(estado)
+		{
+			case INICIO:
+
+				xSemaphoreGive(init);
+
+				if(xSemaphoreTake(init_OK,DELAY)==pdTRUE)
+					estado=LECT_COLOR;
+
+			case LECT_COLOR:
+
+				xSemaphoreGive(sensor_color);
+
+				if(xQueueReceive(color,&data,DELAY)==pdTRUE)
+				{
+					if(data==NON)
+					{
+						cont++;
+
+						if(cont==5)
+							estado=ALARMA;
+						else
+							estado=MOV_MOTOR1;
+					}
+					else
+					{
+						cont=0;
+
+						colores[data-1]++;
+
+						xQueueSend(contador,&colores,DELAY);
+
+						estado=MOV_MOTOR2;
+
+					}
+				}
+
+			case MOV_MOTOR1:
+
+				xSemaphoreGive(motor1);
+
+				if(xSemaphoreTake(motor1_OK,DELAY)==pdTRUE)
+					estado=LECT_COLOR;
+
+			case MOV_MOTOR2:
+
+				xQueueSend(posicion,&data,DELAY);
+
+				if(xSemaphoreTake(motor2_OK,DELAY)==pdTRUE)
+					estado=MOV_MOTOR1;
+
+			case ALARMA:
+
+				xSemaphoreGive(alarma);
+
+				if(xSemaphoreTake(alarma_OK,DELAY)==pdTRUE)
+				{
+
+					cont=0;
+					estado=MOV_MOTOR1;
+
+				}
+		}
+	}
+
+	vTaskDelete(NULL);
+}
+
+static void Inicializacion (void *p)
 {
 
 	while(1)
@@ -117,9 +157,11 @@ static void Inicializacion ()
 			xSemaphoreGive(motor1);
 		}
 	}
+
+	vTaskDelete(NULL);
 }
 
-static void Motor1()
+static void Motor1(void *p)
 {
 
 	while(1)
@@ -130,10 +172,13 @@ static void Motor1()
 		}
 
 	}
+
+	vTaskDelete(NULL);
 }
 
-static void Motor1_STOP()
+static void Motor1_STOP(void *p)
 {
+
 	while(1)
 	{
 		if(xSemaphoreTake(motor1_ISR,DELAY)==pdTRUE)		//tomo semáforo de la interrupcion ef hall
@@ -144,13 +189,14 @@ static void Motor1_STOP()
 		}
 	}
 
+	vTaskDelete(NULL);
+
 }
 
-
-static void Motor2()
+static void Motor2(void *p)
 {
 
-	int pos=NON;
+	static int pos=NON;
 
 	while(1)
 	{
@@ -161,10 +207,13 @@ static void Motor2()
 			xSemaphoreGive(motor2_OK);
 		}
 	}
+
+	vTaskDelete(NULL);
 }
 
-static void Alarma()
+static void Alarma(void *p)
 {
+
 	while(1)
 	{
 		if(xSemaphoreTake(alarma,DELAY)==pdTRUE)
@@ -172,11 +221,14 @@ static void Alarma()
 			activar_Alarma();
 		}
 	}
+
+	vTaskDelete(NULL);
 }
 
-static void Sensor_Color()
+static void Sensor_Color(void *p)
 {
-	int dato;
+
+	static int dato;
 
 	while(1)
 	{
@@ -187,10 +239,13 @@ static void Sensor_Color()
 			xQueueSend(color,&dato,DELAY);
 		}
 	}
+
+	vTaskDelete(NULL);
 }
 
-static void SD()
+static void SD(void *p)
 {
+
 	int cont[5];
 
 	while(1)
@@ -201,88 +256,14 @@ static void SD()
 
 		}
 	}
-}
 
-static void Tarea_Ppal()
-{
-	int estado=INICIO;
-	int data=NON;
-	int cont=0;
-	int i;
-
-	int colores[5];
-
-	for(i=0;i<5;i++)
-		colores[i]=0;
-
-	switch(estado)
-	{
-		case INICIO:
-
-			xSemaphoreGive(init);
-
-			if(xSemaphoreTake(init_OK,DELAY)==pdTRUE)
-				estado=LECT_COLOR;
-
-		case LECT_COLOR:
-
-			xSemaphoreGive(sensor_color);
-
-			if(xQueueReceive(color,&data,DELAY)==pdTRUE)
-			{
-				if(data==NON)
-				{
-					cont++;
-
-					if(cont==5)
-						estado=ALARMA;
-					else
-						estado=MOV_MOTOR1;
-				}
-				else
-				{
-					cont=0;
-
-					colores[data-1]++;
-
-					xQueueSend(contador,&colores,DELAY);
-
-					estado=MOV_MOTOR2;
-
-				}
-			}
-
-		case MOV_MOTOR1:
-
-			xSemaphoreGive(motor1);
-
-			if(xSemaphoreTake(motor1_OK,DELAY)==pdTRUE)
-				estado=LECT_COLOR;
-
-		case MOV_MOTOR2:
-
-			xQueueSend(posicion,&data,DELAY);
-
-			if(xSemaphoreTake(motor2_OK,DELAY)==pdTRUE)
-				estado=MOV_MOTOR1;
-
-		case ALARMA:
-
-			xSemaphoreGive(alarma);
-
-			if(xSemaphoreTake(alarma_OK,DELAY)==pdTRUE)
-				estado=MOV_MOTOR1;
-	}
-
+	vTaskDelete(NULL);
 }
 
 
+int main(void){
 
 
-/*==================[external functions definition]==========================*/
-
-int main(void)
-{
 	initHardware();
 
 	vSemaphoreCreateBinary(init);
@@ -292,8 +273,6 @@ int main(void)
 	vSemaphoreCreateBinary(sensor_color);
 	vSemaphoreCreateBinary(sd);
 	vSemaphoreCreateBinary(motor1_ISR);
-
-
 
 	vSemaphoreCreateBinary(init_OK);
 	vSemaphoreCreateBinary(motor1_OK);
@@ -307,19 +286,34 @@ int main(void)
 	contador= xQueueCreate(5,sizeof(int));
 
 
-	xTaskCreate(Tarea_Ppal, (const char *)"tareappal", configMINIMAL_STACK_SIZE*2, NULL, tskIDLE_PRIORITY+1, 0);
-	xTaskCreate(Inicializacion,(const char *)"inic",configMINIMAL_STACK_SIZE*2, NULL, tskIDLE_PRIORITY+2,0);
-	xTaskCreate(Motor1,(const char *)"motor1",configMINIMAL_STACK_SIZE*2, NULL, tskIDLE_PRIORITY+2,0);
-	xTaskCreate(Motor1_STOP,(const char *)"motor1",configMINIMAL_STACK_SIZE*2, NULL, tskIDLE_PRIORITY+3,0);
-	xTaskCreate(Motor2,(const char *)"motor2",configMINIMAL_STACK_SIZE*2, NULL, tskIDLE_PRIORITY+2,0);
-	xTaskCreate(Alarma,(const char *)"alarm",configMINIMAL_STACK_SIZE*2, NULL, tskIDLE_PRIORITY+2,0);
-	xTaskCreate(Sensor_Color,(const char *)"sensor",configMINIMAL_STACK_SIZE*2, NULL, tskIDLE_PRIORITY+2,0);
-	xTaskCreate(SD,(const char *)"sd",configMINIMAL_STACK_SIZE*2, NULL, tskIDLE_PRIORITY+1,0);
+	if(!init || !motor1 || !motor2 || !alarma || ! sensor_color || !sd || !motor1_ISR || !init_OK
+			|| !motor1_OK || !motor2_OK || !alarma_OK || !sd_OK || !color || !posicion  || !contador)
 
-
-	vTaskStartScheduler();
-
-	while (1) {
+	{
+		return 0;
 	}
+	else
+	{
+		if(!xTaskCreate(Tarea_Ppal,(const char *)"tareappal", configMINIMAL_STACK_SIZE*2, NULL, tskIDLE_PRIORITY+1, 0)
+		|| !xTaskCreate(Inicializacion,(const char *)"inic",configMINIMAL_STACK_SIZE*2, NULL, tskIDLE_PRIORITY+2,0)
+		|| !xTaskCreate(Motor1,(const char *)"motor1",configMINIMAL_STACK_SIZE*2, NULL, tskIDLE_PRIORITY+2,0)
+		|| !xTaskCreate(Motor1_STOP,(const char *)"motor1",configMINIMAL_STACK_SIZE*2, NULL, tskIDLE_PRIORITY+3,0)
+		|| !xTaskCreate(Motor2,(const char *)"motor2",configMINIMAL_STACK_SIZE*2, NULL, tskIDLE_PRIORITY+2,0)
+		|| !xTaskCreate(Alarma,(const char *)"alarm",configMINIMAL_STACK_SIZE*2, NULL, tskIDLE_PRIORITY+2,0)
+		|| !xTaskCreate(Sensor_Color,(const char *)"sensor",configMINIMAL_STACK_SIZE*2, NULL, tskIDLE_PRIORITY+2,0)
+		|| !xTaskCreate(SD,(const char *)"sd",configMINIMAL_STACK_SIZE*2, NULL, tskIDLE_PRIORITY+1,0) )
+		{
+
+			return 0;
+		}
+		else
+			vTaskStartScheduler();
+	}
+
+
+    while(1) {
+
+    }
+    return 0 ;
 }
 
